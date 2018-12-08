313d598d295fb7c263bdba0e0d4d0dfd628cc8ff
Sathyananth <bsathyananth@gmail.com>
2018-12-08 11:16:58 +0530

    Initial Commit

---
+++ src/com/servlet/test/FormParams.java
@@ -0,0 +1,46 @@
+package com.servlet.test;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class FormParams
+ */
+@WebServlet("/FormParams")
+public class FormParams extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public FormParams() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		RequestDispatcher view = request.getRequestDispatcher("HTML/Start.html");
+		view.forward(request, response);
+		
+//		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		System.out.println("Cming hre");
+		System.out.println("Test : " + request.getParameter("chemistry"));
+	}
+
+}
